f418c1a68baff645244b9610d9fcf5b861173c94
mopsyatin <mopsyatin@gmail.com>
2023-10-04 12:57:58 +0200

    added text

--- names.py
+++ names.py
@@ -1,23 +1,23 @@
-shampinion = ""
-lisichka = ""
-opyata = ""
-gruzd = ""
-borowik = ""
-podberez = ""
-podocin = "" 
-ruzhik = ""
-suroezka = ""
-maslenok = ""
-muhomor = ""
-poganka = ""
-satana = ""
-lepiota = ""
-galerina = ""
-pautinic = ""
-zhelchi = ""
-lozhlisichka = ""
-lozhopenek = ""
-mizena = ""
+shampinion = open("1шампньон.txt", "r")
+lisichka = open("2лисичка обыкновенная.txt", "r")
+opyata = open("3опята.txt", "r")
+gruzd = open("4груздь.txt", "r")
+borowik = open("5белый гриб (боровик).txt", "r")
+podberez = open("6подберёзовик.txt", "r")
+podocin = open("7подосиновик.txt", "r")
+ruzhik = open("8рыжик.txt", "r")
+suroezka = open("9сыроежка.txt", "r")
+maslenok = open("10маслёнок.txt", "r")
+muhomor = open("11мухомор.txt", "r")
+poganka = open("12бледная поганка.txt", "r")
+satana = open("13сатанинский гриб.txt", "r")
+lepiota = open("14лепиота коричнево-красная.txt", "r")
+galerina = open("15галерина окаймлённая.txt", "r")
+pautinic = open("16паутинник красивейший.txt", "r")
+zhelchi = open("17желчный гриб.txttxt", "r")
+lozhlisichka = open("18ложная лисичка.txt.txt", "r")
+lozhopenek = open("19ложный опёнок.txt", "r")
+mizena = open("20мицена.txt", "r")
 
 
 
